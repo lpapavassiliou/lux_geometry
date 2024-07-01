e850c4b7592d9e73b78a59a22437150eb8b372e0
lpapavassiliou <ludovico.papavassiliou@gmail.com>
2024-07-01 15:11:17 -0400

    feat(plotting): implemented frame, box and force plots

--- plotter.py
+++ plotter.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-
+from lux_geometry.point3D import Point3D
+from lux_geometry.rotations import HomogeneusMatrix
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 class Visual3D:
     def __init__(self) -> None: # Init method creates a new figure
         self.ax = plt.figure().add_subplot(111, projection='3d')
+        self.frame_scaling = 1.0
+        self.force_scaling = 0.02
 
     def plot_point(self, point):
         self.ax.scatter(point[0], point[1], point[2], c='r', marker='o')
@@ -49,6 +53,22 @@ class Visual3D:
         for vector in vectors:
             self.plot_vector(*vector, color=color,linewidth=linewidth)
 
+    @staticmethod
+    def build_frame(World_T_Frame:HomogeneusMatrix, scale=1.0):
+        World_newOrigin = World_T_Frame.get_t()
+        R = scale * World_T_Frame.get_R()
+        World_newXAxisEnd = R[:,0] + World_newOrigin
+        World_newYAxisEnd = R[:,1] + World_newOrigin
+        World_newZAxisEnd = R[:,2] + World_newOrigin
+        lines = [[World_newOrigin, World_newXAxisEnd],
+                [World_newOrigin, World_newYAxisEnd],
+                [World_newOrigin, World_newZAxisEnd]]
+        return lines
+
+    def plot_frame(self, World_T_Frame : HomogeneusMatrix, color=None):
+        lines_Frame = self.build_frame(World_T_Frame, scale=self.frame_scaling)
+        self.plot_vectors(lines_Frame, color=color)
+
     def set_ax_limits(self, dim, offset=[0, 0, 0]):
         x_lim = [-dim + offset[0], dim + offset[0]]
         y_lim = [-dim + offset[1], dim + offset[1]]
@@ -76,6 +96,27 @@ class Visual3D:
         self.ax.set_title('3D Scene')
         plt.show()
     
+    def plot_force(self, Frame_forceLocation : Point3D, Frame_forceVector, World_T_Frame : HomogeneusMatrix = None, linewidth=3):
+        if World_T_Frame is None:
+            World_T_Frame = HomogeneusMatrix()
+        World_forceOrigin = World_T_Frame.transform(Frame_forceLocation)
+        World_forceEnd = World_T_Frame.transform(Frame_forceLocation + self.force_scaling * Frame_forceVector)
+        self.plot_vector(World_forceOrigin, World_forceEnd, linewidth=linewidth)
+    
+    def plot_box(self, points):
+        # Define the 6 faces of the box using the points
+        faces = [
+            [points[0], points[1], points[2], points[3]],  # Bottom face
+            [points[4], points[5], points[6], points[7]],  # Top face
+            [points[0], points[1], points[5], points[4]],  # Front face
+            [points[2], points[3], points[7], points[6]],  # Back face
+            [points[1], points[2], points[6], points[5]],  # Right face
+            [points[4], points[7], points[3], points[0]]   # Left face
+        ]
+        # Plot the faces
+        self.ax.add_collection3d(Poly3DCollection(faces, facecolors='black', linewidths=1, edgecolors='black', alpha=.25))
+
+
     @staticmethod
     def update_animation():
         plt.draw()
